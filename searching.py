16c71a01f9c9596cc59160faa39e75b2d0afd789
juraj271353 <271353@vutbr.cz>
2026-04-16 15:37:32 +0200

    commit2

--- searching.py
+++ searching.py
@@ -13,8 +13,25 @@ def read_data(file_name, key):
         return None
 
 
+def linear_search(seq, number):
+    count = 0
+    positions = []
+
+    for index,num in enumerate(seq):
+        if num == number:
+            positions.append(index)
+            count += 1
+
+    return {
+        "positions": positions,
+        "count": count
+    }
+
+
+
 def main():
     print(read_data("sequential.json", "unordered_numbers"))
+    print(linear_search([54, 2, 18, -10, 300, 17, 5, -1, 0, 0, 102, 7, 8, 9, 9, -3, -5, 0, 1, 63, 82, -36, -5], 0))
 
 
 if __name__ == "__main__":
